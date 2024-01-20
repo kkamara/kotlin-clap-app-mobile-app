b9ece5bb7facf9ade8efd22aa4868d645755b15c
kkamara <kamaracomputers@gmail.com>
2024-01-20 14:33:31 +0000

    Add working play, pause and stop buttons

--- app/src/main/java/com/kelvinkamara/clapapp/MainActivity.kt
+++ app/src/main/java/com/kelvinkamara/clapapp/MainActivity.kt
@@ -4,18 +4,36 @@ import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mediaPlayer: MediaPlayer
+    private var mediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mediaPlayer = MediaPlayer.create(this, R.raw.applauding)
 
-        val button = findViewById<Button>(R.id.btnClap)
-        button.setOnClickListener{
-            mediaPlayer.start()
+        val playButton = findViewById<FloatingActionButton>(R.id.fabPlay)
+        val pauseButton = findViewById<FloatingActionButton>(R.id.fabPause)
+        val stopButton = findViewById<FloatingActionButton>(R.id.fabStop)
+        playButton.setOnClickListener {
+            if (mediaPlayer == null) {
+                mediaPlayer = MediaPlayer.create(
+                    this,
+                    R.raw.applauding
+                )
+            }
+            mediaPlayer?.start()
+        }
+        pauseButton.setOnClickListener {
+            mediaPlayer?.pause()
+        }
+        stopButton.setOnClickListener {
+            mediaPlayer?.stop()
+            mediaPlayer?.reset()
+            mediaPlayer?.release()
+            mediaPlayer = null
         }
     }
 }
